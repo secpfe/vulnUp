6b0db0ca6dcccfbf0eaee0a3e9e752b30e8c0ac3
Nikolay Salnikov <nsalnikov@gmail.com>
2024-10-02 20:48:59 +0400

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php
-// Displays information about the current PHP setup
-phpinfo();
+// Vulnerable to LFI
+if (isset($_GET['page'])) {
+    $page = $_GET['page'];
+
+    // Basic path traversal protection (weak), can be bypassed by ../
+    if (strpos($page, '..') === false) {
+        // Include the requested page
+        include($page . ".php");
+    } else {
+        echo "Invalid page.";
+    }
+} else {
+    echo "Welcome to the homepage! Use the 'page' parameter to navigate.";
+}
 ?>
