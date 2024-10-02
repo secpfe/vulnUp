989f88bf53adc54ed6a701c67a1a8753145e3fd6
Nikolay Salnikov <nsalnikov@gmail.com>
2024-10-02 21:18:46 +0400

    Add files via upload

---
+++ pages/services.php
@@ -0,0 +1,11 @@
+<?php
+echo "<h1>Our Services</h1>";
+echo "<p>We offer a wide range of web security services to help you build and maintain secure web applications. Our services include:</p>";
+echo "<ul>";
+echo "<li>Web Application Vulnerability Assessments</li>";
+echo "<li>Security Code Reviews</li>";
+echo "<li>Penetration Testing</li>";
+echo "<li>Custom Web Security Training</li>";
+echo "</ul>";
+echo "<p>Contact us today to learn more about how we can help secure your web applications.</p>";
+?>
